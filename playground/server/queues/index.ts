59f69cdf4417a408b2e8e9d39c4d7bafbd14c9d4
Aidan Hibbard <aidanchibbard@gmail.com>
2025-08-31 20:18:17 -0700

    fix set interval in playground

--- playground/server/queues/index.ts
+++ playground/server/queues/index.ts
@@ -4,8 +4,8 @@ const queue = defineQueue({
   name: 'hello',
 })
 
-setInterval(() => {
-  queue.add('hello', { message: 'hello', ts: Date.now() })
+setInterval(async () => {
+  await queue.add('hello', { message: 'hello', ts: Date.now() })
 }, 5000)
 
 export default queue
