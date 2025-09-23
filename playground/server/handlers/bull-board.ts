dbf48b53a4c787f9f45f4764343fff8639d29c0d
Aidan Hibbard <aidanchibbard@gmail.com>
2025-09-23 13:14:13 -0700

    Update module.ts

--- playground/server/handlers/bull-board.ts
+++ playground/server/handlers/bull-board.ts
@@ -3,17 +3,19 @@ import { BullMQAdapter } from '@bull-board/api/bullMQAdapter'
 import { H3Adapter } from '@bull-board/h3'
 import type { H3Event } from 'h3'
 import HelloQueue from '../queues'
+import BasicQueue from '../queues/basic'
 
 const serverHandler = new H3Adapter()
 serverHandler.setBasePath('/bull-board')
 
 createBullBoard({
-  queues: [new BullMQAdapter(HelloQueue)],
+  queues: [
+    new BullMQAdapter(HelloQueue),
+    new BullMQAdapter(BasicQueue),
+  ],
   serverAdapter: serverHandler,
 })
 
 const uiHandler = serverHandler.registerHandlers()
 
-export const redirectToBullboard = async (event: H3Event) => {
-  return await uiHandler.handler(event)
-}
+export const redirectToBullboard = async (event: H3Event) => await uiHandler.handler(event)
